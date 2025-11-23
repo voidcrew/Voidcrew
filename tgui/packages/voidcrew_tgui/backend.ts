9906e8a3914c05b14ad9ac92ea91deb81e60ccc9
jackriip <jackriiip@gmail.com>
2025-11-23 01:29:30 -0800

    working voidcrew uis

---
+++ tgui/packages/voidcrew_tgui/backend.ts
@@ -0,0 +1,5 @@
+/**
+ * Re-export backend from main tgui package
+ * This allows voidcrew interfaces to import from '../backend'
+ */
+export * from '../tgui/backend';
---
+++ tgui/packages/voidcrew_tgui/layouts.ts
@@ -0,0 +1,5 @@
+/**
+ * Re-export layouts from main tgui package
+ * This allows voidcrew interfaces to import from '../layouts'
+ */
+export * from '../tgui/layouts';
