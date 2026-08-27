tgstation/tgstation

commit e365caa54a3619cef715f5517830f36e9f656d85

Command bar starts on Say & clears on Backspace (#97567)
## About The Pull Request

Commandbar now starts the round on Say mode, and hitting backspace
deletes the whole thing but still resets to the mode selected once you
do your next command bar action, both of these were how the command bar
worked previously so I am bringing back this functionality.

## Why It's Good For The Game

Bringing back previous functionality that I think worked better, for the
new tgui command bar.

## Changelog

:cl:
qol: Pressing backspace while unfocused now clears your command bar.
qol: The Command bar now starts on Say instead of Command mode.
/:cl:

tgui/packages/tgui-panel/verbs/CommandBar.tsx
@@ -14,6 +14,7 @@ import {
   clearCommandBarAtom,
   focusCommandBarAtom,
   hotkeysAtom,
+  initializeCommandBarAtom,
   typepathsAtom,
   type Verb,
   type VerbArg,
@@ -177,11 +178,13 @@ export function CommandBar() {
   const focusSignal = useAtomValue(focusCommandBarAtom);
   const clearSignal = useAtomValue(clearCommandBarAtom);
   const hotkeys = useAtomValue(hotkeysAtom);
+  const initializeSignal = useAtomValue(initializeCommandBarAtom);
   const [input, setInput] = useState('');
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [selectedVerb, setSelectedVerb] = useState<Verb | null>(null);
   const [filledArgs, setFilledArgs] = useState<string[]>([]);
-  const [mode, setMode] = useState<Mode>('Command');
+  const [lastTypepathRequest, setLastTypepathRequest] = useState('');
+  const [mode, setMode] = useState<Mode>('Say');
   const inputRef = useRef<HTMLInputElement>(null);
   const historyRef = useRef<string[]>([]);
   const [historyIndex, setHistoryIndex] = useState(-1);
@@ -217,6 +220,12 @@ export function CommandBar() {
   }, []);
 
   useEffect(() => {
+    if (mode !== 'Command') {
+      enterChatMode(mode);
+    }
+  }, [initializeSignal]);
+
+  useEffect(() => {
     if (focusSignal > 0) {
       inputRef.current?.focus();
     }
@@ -230,11 +239,7 @@ export function CommandBar() {
 
   useEffect(() => {
     if (clearSignal > 0) {
-      if (mode !== 'Command') {
-        enterChatMode(mode);
-      } else {
-        resetState();
-      }
+      resetState();
     }
   }, [clearSignal]);
 

tgui/packages/tgui-panel/verbs/atoms.ts
@@ -21,4 +21,5 @@ export const adminTargetsAtom = atom<Target[]>([]);
 export const typepathsAtom = atom<string[]>([]);
 export const focusCommandBarAtom = atom<number>(0);
 export const clearCommandBarAtom = atom<number>(0);
+export const initializeCommandBarAtom = atom<number>(0);
 export const hotkeysAtom = atom<boolean>(true);

tgui/packages/tgui-panel/verbs/handlers.ts
@@ -7,6 +7,7 @@ import {
   clearCommandBarAtom,
   focusCommandBarAtom,
   hotkeysAtom,
+  initializeCommandBarAtom,
   type Target,
   typepathsAtom,
   type Verb,
@@ -29,6 +30,7 @@ function loadTypepaths() {
 
 export function handleVerbsInit(payload: { verbs: Verb[] }) {
   store.set(adminVerbsAtom, payload.verbs || []);
+  store.set(initializeCommandBarAtom, (n) => n + 1);
   loadTypepaths();
 }
 
@@ -54,7 +56,6 @@ export function handleRemoveVerbs(payload: { names: string[] }) {
 export function handleTargets(payload: { targets: Target[] }) {
   store.set(adminTargetsAtom, payload.targets || []);
 }
-
 
 export function handleFocusCommandBar() {
   store.set(focusCommandBarAtom, (n) => n + 1);

voidcrew/Voidcrew

commit 2bfeb8d343bfcbc6f0a6ba305f67cbee41105231

Removes merge conflict with command bar (#97630)
## About The Pull Request

Benign, minor, irrelevant. Changes nothing, yet here we are.

Caused by https://github.com/tgstation/tgstation/pull/97567 removed by
https://github.com/tgstation/tgstation/pull/97529

tgui/packages/tgui-panel/verbs/CommandBar.tsx
@@ -183,7 +183,6 @@ export function CommandBar() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [selectedVerb, setSelectedVerb] = useState<Verb | null>(null);
   const [filledArgs, setFilledArgs] = useState<string[]>([]);
-  const [lastTypepathRequest, setLastTypepathRequest] = useState('');
   const [mode, setMode] = useState<Mode>('Say');
   const inputRef = useRef<HTMLInputElement>(null);
   const historyRef = useRef<string[]>([]);
@@ -555,7 +554,6 @@ export function CommandBar() {
         }
       }
     }
-
   };
 
   const placeholder = selectedVerb